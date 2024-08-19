4579797944d4a3f49f19116b5b7a7eb71de3393b
Varun gupta <varun3dec@gmail.com>
2024-08-19 13:41:53 +0530

    second commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='mlprojects',
+    version='0.0.1',
+    author='VG',
+    packages=find_packages(),
+    install_requires=['pandas','numpy','seaborn']
+
+)
